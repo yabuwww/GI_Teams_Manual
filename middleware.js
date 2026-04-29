06e9b70dad5a282ad7851ce7885842aedde14ca6
yabu <yabuhara.ayumu@gmail.com>
2026-04-30 00:45:40 +0900

    今度こそパスワード

--- middleware.js
+++ middleware.js
@@ -6,7 +6,7 @@ export default function middleware(req) {
   const authHeader = req.headers.get('authorization');
 
   // ※もしパスワードを変えたい場合は、ここを新しい文字列に差し替えてください
-  if (authHeader !== 'bW9ldGE6bW9lbW9la3l1bg==') {
+  if (authHeader !== 'Basic bW9ldGE6bW9lbW9la3l1bg==') {
     return new Response('認証が必要です', {
       status: 401,
       headers: {
